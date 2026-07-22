b394ca73d9cfc4c44e5df49b0995b3dc9833d2e3
kato000001 <250024jf@yse-c.net>
2026-07-22 12:04:19 +0900

    SQL最新にしておいた

--- clip.sql
+++ clip.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jul 21, 2026 at 03:12 AM
+-- Generation Time: Jul 22, 2026 at 03:03 AM
 -- Server version: 8.4.3
 -- PHP Version: 8.3.30
 
@@ -41,10 +41,6 @@ CREATE TABLE `boards` (
 --
 
 INSERT INTO `boards` (`id`, `user_id`, `name`, `sort_order`, `created_at`, `updated_at`) VALUES
-(36, 1, '絵師向けの使用例１', 0, '2026-07-13 15:48:42', '2026-07-14 13:36:49'),
-(44, 1, 'テスト１', 0, '2026-07-14 09:06:38', '2026-07-14 11:53:51'),
-(54, 1, '２', 0, '2026-07-14 10:46:59', '2026-07-14 10:46:59'),
-(55, 1, '３', 0, '2026-07-14 10:47:02', '2026-07-14 10:47:02'),
 (56, 1, '４', 0, '2026-07-14 10:47:05', '2026-07-14 10:47:05'),
 (66, 1, 'さかな', 0, '2026-07-14 13:46:48', '2026-07-14 13:46:48');
 
@@ -59,13 +55,6 @@ CREATE TABLE `board_tags` (
   `tag_id` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `board_tags`
---
-
-INSERT INTO `board_tags` (`board_id`, `tag_id`) VALUES
-(36, 3);
-
 -- --------------------------------------------------------
 
 --
@@ -79,18 +68,9 @@ CREATE TABLE `cards` (
   `pos_x` int NOT NULL DEFAULT '0',
   `pos_y` int NOT NULL DEFAULT '0',
   `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `content` text
+  `content` text,
+  `z_index` int NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `cards`
---
-
-INSERT INTO `cards` (`id`, `board_id`, `type`, `pos_x`, `pos_y`, `created_at`, `content`) VALUES
-(111, 36, 'url', 190, 431, '2026-07-13 16:04:40', NULL),
-(126, 36, 'text', 214, 132, '2026-07-13 16:14:14', '作業中に聞く配信'),
-(127, 36, 'url', 168, 174, '2026-07-13 16:15:07', NULL),
-(142, 44, 'url', 145, 68, '2026-07-14 09:10:53', NULL);
 
 -- --------------------------------------------------------
 
@@ -118,15 +98,6 @@ CREATE TABLE `card_urls` (
   `thumbnail_url` varchar(2083) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
---
--- Dumping data for table `card_urls`
---
-
-INSERT INTO `card_urls` (`id`, `card_id`, `url`, `title`, `thumbnail_url`) VALUES
-(8, 111, 'https://x.com/nyum_serori', 'にゅむ＠c108_2日目西ひ-01ab (@nyum_serori) on X', 'https://pbs.twimg.com/profile_images/1886273472743247872/gJoAe6nN_200x200.jpg'),
-(14, 127, 'https://www.youtube.com/live/kA4IybS21Kc?si=MRTL4BLQtgKnnmKG', '【#しぐれういスプリング2026】安価で春服考えようぜ', 'https://i.ytimg.com/vi/kA4IybS21Kc/maxresdefault.jpg'),
-(18, 142, 'https://youtu.be/mhWYv-Difv8?si=HmcMiz0KXIE7CeN0', 'どうしてもオタクを風呂に入れたいコラボ銭湯【学マス×極楽湯】', 'https://i.ytimg.com/vi/mhWYv-Difv8/maxresdefault.jpg');
-
 -- --------------------------------------------------------
 
 --
@@ -146,10 +117,11 @@ CREATE TABLE `tags` (
 --
 
 INSERT INTO `tags` (`id`, `user_id`, `name`, `color`, `created_at`) VALUES
-(3, 1, 'キャラデザ', '#E05555', '2026-07-16 06:10:39'),
-(5, 1, '７月', '#9955E0', '2026-07-21 00:43:52'),
-(6, 1, 'ノルムー', '#5588E0', '2026-07-21 00:44:12'),
-(7, 1, 'ゼンゼロ', '#55A855', '2026-07-21 00:44:28');
+(10, 1, '７月', '#EBB73E', '2026-07-22 02:53:04'),
+(11, 1, '8月', '#E05555', '2026-07-22 02:53:24'),
+(12, 1, '9月', '#E05555', '2026-07-22 02:53:29'),
+(13, 1, '10月', '#EBB73E', '2026-07-22 02:53:36'),
+(14, 1, '11月', '#EBB73E', '2026-07-22 02:53:44');
 
 -- --------------------------------------------------------
 
@@ -227,31 +199,31 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `boards`
 --
 ALTER TABLE `boards`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=74;
 
 --
 -- AUTO_INCREMENT for table `cards`
 --
 ALTER TABLE `cards`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=199;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=227;
 
 --
 -- AUTO_INCREMENT for table `card_images`
 --
 ALTER TABLE `card_images`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=125;
 
 --
 -- AUTO_INCREMENT for table `card_urls`
 --
 ALTER TABLE `card_urls`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=50;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT for table `tags`
 --
 ALTER TABLE `tags`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `users`
